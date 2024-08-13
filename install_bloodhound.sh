92af0d0e256e43ed84baa94c75e2b7b5ab9d0106
vaarg <104344158+vaarg@users.noreply.github.com>
2024-08-13 15:15:14 +1000

    Create install_bloodhound.sh

---
+++ install_bloodhound.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt install -y docker.io
+sudo systemctl enable docker --now
+sudo usermod -aG docker $USER
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list 
+curl -fsSL https://download.docker.com/linux/debian/gpg |
+  sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo apt update
+sudo apt update
+
+echo "Yehaaawww"
+echo "Now run:"
+echo "curl -L https://ghst.ly/getbhce | sudo docker compose -f - up"
